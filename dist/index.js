c80a01d6207207db3e89735cd8e410b8183af6d3
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2025-10-30 11:57:06 +0100

    myErrors

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const app = (0, express_1.default)();
+import express from "express";
+const app = express();
 const PORT = 7777;
 app.listen(PORT, () => {
     console.log(`App is running on Port ${PORT}`);
+});
+app.get('/', (req, res) => {
+    res.send("Holla .ts!!!");
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = 7777
@@ -6,3 +6,7 @@ const PORT = 7777
 app.listen(PORT, () => {
   console.log(`App is running on Port ${PORT}`)
 });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Holla .ts!!!")
+})
